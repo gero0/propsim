30ba5fa7ff0bd2ee1881d1329ab6af7d1d2da064
Kacper <kacper.lesnianski@wp.pl>
2022-10-20 00:01:44 +0200

    cuda conditional compilation

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#ifdef CUDA_AVAL
 #include "cudamatmul.h"
+#endif
 
 int main(void)
 {
+#ifdef CUDA_AVAL
     cudaMul();
+#endif
     std::cout << "Hello world";
     return 0;
 }
